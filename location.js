b48f2d247ed0f8a6315bc94e409cbcac74d02601
unStatiK <unstatik@staremax.com>
2013-04-04 21:03:02 +0700

    upload static files

---
+++ location.js
@@ -0,0 +1,29 @@
+  ymaps.ready(init);
+
+        function init () {
+
+            var myMap = new ymaps.Map("map", {
+
+                    center: [53.336314, 83.778545],
+
+                    zoom: 16
+                }),
+
+                myPlacemark = new ymaps.Placemark([53.336390, 83.778310], {
+
+                    iconContent: "НПК 'Staremax group'"
+
+                }, {
+
+                    preset: 'twirl#greyStretchyIcon'
+
+                });
+
+
+
+                myMap.geoObjects
+
+                .add(myPlacemark);
+
+
+        }
